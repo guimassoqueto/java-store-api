4ecb214987c661e05ad12ea3af296342ae5d2d29
guimassoqueto <gui.massoqueto@gmail.com>
2023-05-22 15:59:10 -0300

    feat: automatic instanciation on init application

--- src/main/java/com/guimassoqueto/storeapi/storeapi/StoreapiApplication.java
+++ src/main/java/com/guimassoqueto/storeapi/storeapi/StoreapiApplication.java
@@ -1,13 +1,31 @@
 package com.guimassoqueto.storeapi.storeapi;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.guimassoqueto.storeapi.storeapi.domain.Categoria;
+import com.guimassoqueto.storeapi.storeapi.repositories.CategoriaRepository;
+
 @SpringBootApplication
-public class StoreapiApplication {
+public class StoreapiApplication implements CommandLineRunner {
+	
+	@Autowired
+	private CategoriaRepository catRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(StoreapiApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+
+		catRepository.saveAll(Arrays.asList(cat1, cat2));
 	}
 
 }
