2b79241e09de7df8d813324f76819891342ed4aa
guimassoqueto <gui.massoqueto@gmail.com>
2023-05-29 07:21:01 -0300

    feat: Cliente, TipoCliente, Endereco

--- src/main/java/com/guimassoqueto/storeapi/storeapi/StoreapiApplication.java
+++ src/main/java/com/guimassoqueto/storeapi/storeapi/StoreapiApplication.java
@@ -9,10 +9,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.guimassoqueto.storeapi.storeapi.domain.Categoria;
 import com.guimassoqueto.storeapi.storeapi.domain.Cidade;
+import com.guimassoqueto.storeapi.storeapi.domain.Cliente;
+import com.guimassoqueto.storeapi.storeapi.domain.Endereco;
 import com.guimassoqueto.storeapi.storeapi.domain.Estado;
 import com.guimassoqueto.storeapi.storeapi.domain.Produto;
+import com.guimassoqueto.storeapi.storeapi.domain.enums.TipoCliente;
 import com.guimassoqueto.storeapi.storeapi.repositories.CategoriaRepository;
 import com.guimassoqueto.storeapi.storeapi.repositories.CidadeRepository;
+import com.guimassoqueto.storeapi.storeapi.repositories.ClienteRepository;
+import com.guimassoqueto.storeapi.storeapi.repositories.EnderecoRepository;
 import com.guimassoqueto.storeapi.storeapi.repositories.EstadoRepository;
 import com.guimassoqueto.storeapi.storeapi.repositories.ProdutoRepository;
 
@@ -30,6 +35,12 @@ public class StoreapiApplication implements CommandLineRunner {
 
 	@Autowired
 	private EstadoRepository estadoRepository;
+
+	@Autowired
+	private ClienteRepository clienteRepository;
+
+	@Autowired
+	private EnderecoRepository enderecoRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(StoreapiApplication.class, args);
@@ -51,6 +62,11 @@ public class StoreapiApplication implements CommandLineRunner {
 		Cidade c2 = new Cidade(null, "São Paulo", est2);
 		Cidade c3 = new Cidade(null, "Campinas", est2);
 
+		Cliente cli1 = new Cliente(null, "Maria Silva", "maria@gmail.com", "36378912377", TipoCliente.PESSOAFISICA);
+		
+		Endereco e1 = new Endereco(null, "Rua Flores", "300", "Apto 303", "Jardim", "38220834", c1, cli1);
+		Endereco e2 = new Endereco(null, "Avenida Matos", "105", "Sala 800", "Centro", "38777012", c1, cli1);
+
 		cat1.getProdutos().addAll(Arrays.asList(p1, p2, p3));
 		cat2.getProdutos().addAll(Arrays.asList(p2));
 
@@ -61,11 +77,17 @@ public class StoreapiApplication implements CommandLineRunner {
 		est1.getCidades().addAll(Arrays.asList(c1));
 		est2.getCidades().addAll(Arrays.asList(c2, c3));
 
+		cli1.getEnderecos().addAll(Arrays.asList(e1, e2));
+		cli1.getTelefones().addAll(Arrays.asList("27363323", "93838393"));
+
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
 
 		estadoRepository.saveAll(Arrays.asList(est1, est2));
 		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
+
+		clienteRepository.saveAll(Arrays.asList(cli1));
+		enderecoRepository.saveAll(Arrays.asList(e1, e2));
 	}
 
 }
---
+++ src/main/java/com/guimassoqueto/storeapi/storeapi/domain/Cliente.java
@@ -0,0 +1,122 @@
+package com.guimassoqueto.storeapi.storeapi.domain;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import javax.persistence.CollectionTable;
+import javax.persistence.ElementCollection;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+import com.guimassoqueto.storeapi.storeapi.domain.enums.TipoCliente;
+import java.util.Objects;
+
+@Entity
+public class Cliente implements Serializable {
+  private static final long serialVersionUID = 1L;
+  
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Integer id;
+  private String name;
+  private String email;
+  private String cpfOuCnpj;
+  private Integer tipo;
+  
+  @OneToMany(mappedBy = "cliente")
+  private List<Endereco> enderecos = new ArrayList<>();
+
+  @ElementCollection
+  @CollectionTable(name = "telefone")
+  private Set<String> telefones = new HashSet<>();
+
+  public Cliente() {}
+
+  public Cliente(Integer id, String name, String email, String cpfOuCnpj, TipoCliente tipo) {
+    super();
+    this.id = id;
+    this.name = name;
+    this.email = email;
+    this.cpfOuCnpj = cpfOuCnpj;
+    this.tipo = tipo.getCod();
+  }
+
+  public Integer getId() {
+    return this.id;
+  }
+
+  public void setId(Integer id) {
+    this.id = id;
+  }
+
+  public String getName() {
+    return this.name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getEmail() {
+    return this.email;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
+
+  public String getCpfOuCnpj() {
+    return this.cpfOuCnpj;
+  }
+
+  public void setCpfOuCnpj(String cpfOuCnpj) {
+    this.cpfOuCnpj = cpfOuCnpj;
+  }
+
+  public TipoCliente getTipo() {
+    return TipoCliente.toEnum(this.tipo);
+  }
+
+  public void setTipo(TipoCliente tipo) {
+    this.tipo = tipo.getCod();
+  }
+
+  public List<Endereco> getEnderecos() {
+    return this.enderecos;
+  }
+
+  public void setEnderecos(List<Endereco> enderecos) {
+    this.enderecos = enderecos;
+  }
+
+  public Set<String> getTelefones() {
+    return this.telefones;
+  }
+
+  public void setTelefones(Set<String> telefones) {
+    this.telefones = telefones;
+  }
+
+  @Override
+    public boolean equals(Object o) {
+        if (o == this)
+            return true;
+        if (!(o instanceof Cliente)) {
+            return false;
+        }
+        Cliente cliente = (Cliente) o;
+        return Objects.equals(id, cliente.id) && Objects.equals(name, cliente.name) && Objects.equals(email, cliente.email) && Objects.equals(cpfOuCnpj, cliente.cpfOuCnpj) && Objects.equals(tipo, cliente.tipo) && Objects.equals(enderecos, cliente.enderecos) && Objects.equals(telefones, cliente.telefones);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(id, name, email, cpfOuCnpj, tipo, enderecos, telefones);
+  }
+
+}
